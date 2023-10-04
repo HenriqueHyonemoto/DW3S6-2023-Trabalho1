f23a1b20d3a8f2a1f1d70b2e3f8d687aa8ee58cf
Mary Brachini <92119838+MaryBrachini@users.noreply.github.com>
2023-10-04 09:37:44 -0300

    Update databaseConfig.sql

--- databaseConfig.sql
+++ databaseConfig.sql
@@ -1,21 +1,3 @@
-create table IF NOT EXISTS banco (
-    bancoid bigserial constraint pk_banco PRIMARY KEY,
-    numero_banco varchar(50) UNIQUE,
-    nome VARCHAR(40),
-    data_criacao date,
-    taxa_juros numeric(3,3),
-    ativo boolean,
-    removido boolean DEFAULT false
-);
-
-insert into banco values 
-    (default, '12345', 'Santander','','', true),
-    (default, '54321', 'Bradesco','','', true),
-    (default, '14736', 'Caixa Economica Federal','','', true),
-    (default, '96374', 'Banco do Brasil','','',false)
-    ON CONFLICT DO NOTHING;
-
-
 create table IF NOT EXISTS agencia (
     agenciaid bigserial constraint pk_agencia PRIMARY KEY,
     numero_agencia varchar(5) UNIQUE,
@@ -23,34 +5,15 @@ create table IF NOT EXISTS agencia (
     data_criacao date,
     taxa_transacao numeric(3,3),
     ativo boolean,
-    agenciad bigint constraint fk_agencia_banco REFERENCES banco,
     removido boolean DEFAULT false
 );
 
 insert into agencia values 
-    (default, '001', 'Centro','','', true,(SELECT agenciaid from banco where numero_banco = '12345')),
-    (default, '007', 'Norte','','', true,(SELECT agenciaid from banco where numero_banco = '54321')),
-    (default, '238', 'Dentro do shopping Zona Sul','','', true,(SELECT agenciaid from banco where numero_banco = '14736')),
-    (default, '102', 'Ao lado do mercado Proença','','', false,(SELECT agenciaid from banco where numero_banco = '96374'))
+    (default, '001', 'Centro','','', true),
+    (default, '007', 'Norte','','', true,),
+    (default, '238', 'Dentro do shopping Zona Sul','','', true),
+    (default, '102', 'Ao lado do mercado Proença','','', false)
     ON CONFLICT DO NOTHING;
-
-create table IF NOT EXISTS cliente (
-    clienteid bigserial constraint pk_cliente PRIMARY KEY,
-    conta_corrente varchar(10) UNIQUE,
-    nome varchar(50),
-    cpf VARCHAR(14),
-    renda numeric(8,2),
-    data_nasc date,
-    clienteid bigint constraint fk_cliente_agencia REFERENCES agencia,
-    removido boolean DEFAULT false
-);
-
-insert into cliente values 
-    (default, 'pront1', 'José das Neves', 'Rua A, Votuporanga', 6891.60, '2000-01-31', 
-        (SELECT clienteid from agencia where codigo = '001')),
-    (default, 'pront2', 'Maria Silveira', 'Rua B, São José do Rio Preto', 7372.41, '2002-03-12', 
-        (SELECT clienteid from agencia where codigo = '102'))
-ON CONFLICT DO NOTHING;
 
 --Usuarios do sistema
 create table IF NOT EXISTS usuarios (
