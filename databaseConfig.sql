97fa4d04adc41a22eea200439961a453b37883a4
Mary Brachini <92119838+MaryBrachini@users.noreply.github.com>
2023-10-04 09:22:04 -0300

    Update databaseConfig.sql

--- databaseConfig.sql
+++ databaseConfig.sql
@@ -1,30 +1,34 @@
 create table IF NOT EXISTS banco (
     bancoid bigserial constraint pk_banco PRIMARY KEY,
-    codigo varchar(50) UNIQUE,
-    descricao VARCHAR(60),
+    numbanco varchar(50) UNIQUE,
+    nome VARCHAR(60),
+    date,
+    numeric(,),
     ativo boolean,
     deleted boolean DEFAULT false
 );
 
-insert into agencia values 
-    (default, '001', 'Bacharelado em Sistemas de Informação', true),
-    (default, '007', 'Bacharelado em Direito', true),
-    (default, '238', 'Licenciatura em Letras', true),
-    (default, '102', 'Bacharelado em Administração', false)
+insert into banco values 
+    (default, '1234', 'Santander', true),
+    (default, '4321', 'Bradesco', true),
+    (default, '1473', 'Caixa Economica Federal', true),
+    (default, '9637', 'Banco do Brasil', false)
     ON CONFLICT DO NOTHING;
 
 
 create table IF NOT EXISTS agencia (
     agenciaid bigserial constraint pk_agencia PRIMARY KEY,
-    codigo varchar(50) UNIQUE,
+    numagencia varchar(5) UNIQUE,
     descricao VARCHAR(60),
+    data_criacao date,
+    taxa_transacao numeric(3,3),
     ativo boolean,
     agenciad bigint constraint fk_agencia_banco REFERENCES banco,
     deleted boolean DEFAULT false
 );
 
 insert into agencia values 
-    (default, '001', 'Bacharelado em Sistemas de Informação', true),
+    (default, '001', 'Centro', true,(SELECT agenciaid from banco where numbanco = '001')),
     (default, '007', 'Bacharelado em Direito', true),
     (default, '238', 'Licenciatura em Letras', true),
     (default, '102', 'Bacharelado em Administração', false)
@@ -43,9 +47,9 @@ create table IF NOT EXISTS cliente (
 
 insert into cliente values 
     (default, 'pront1', 'José das Neves', 'Rua A, Votuporanga', 6891.60, '2000-01-31', 
-        (SELECT cursoid from CURSOS where codigo = 'BSI')),
+        (SELECT clienteid from agencia where codigo = '001')),
     (default, 'pront2', 'Maria Silveira', 'Rua B, São José do Rio Preto', 7372.41, '2002-03-12', 
-        (SELECT cursoid from CURSOS where codigo = 'DIREITO'))
+        (SELECT clienteid from agencia where codigo = '102'))
 ON CONFLICT DO NOTHING;
 
 --Usuarios do sistema
