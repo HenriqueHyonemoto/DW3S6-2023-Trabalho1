67e9788f4dc6690d61ad47d7223cdacf876e9241
Mary Brachini <92119838+MaryBrachini@users.noreply.github.com>
2023-10-04 09:42:44 -0300

    Update databaseConfig.sql

--- databaseConfig.sql
+++ databaseConfig.sql
@@ -2,17 +2,18 @@ create table IF NOT EXISTS agencia (
     agenciaid bigserial constraint pk_agencia PRIMARY KEY,
     numero_agencia varchar(5) UNIQUE,
     descricao VARCHAR(60),
+    banco varchar(40),
     data_criacao date,
-    taxa_transacao numeric(3,3),
+    taxa_transacao numeric(1,1),
     ativo boolean,
     removido boolean DEFAULT false
 );
 
 insert into agencia values 
-    (default, '001', 'Centro','','', true),
-    (default, '007', 'Norte','','', true,),
-    (default, '238', 'Dentro do shopping Zona Sul','','', true),
-    (default, '102', 'Ao lado do mercado Proença','','', false)
+    (default, '001', 'Centro','Santander','20/08/2000','1.5', true),
+    (default, '007', 'Norte','Banco do Brasil','04/09/1985','0.5', true,),
+    (default, '238', 'Dentro do shopping Zona Sul','Bradesco','16/03/1974','1.2', true),
+    (default, '102', 'Ao lado do mercado Proença','Caixa Economica Federal','25/06/1988','0.8', false)
     ON CONFLICT DO NOTHING;
 
 --Usuarios do sistema
